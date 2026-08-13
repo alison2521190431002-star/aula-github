26457800826cf47ef019e239fd66c103e7f5ab41
Alison <alison.2521190431002@aluno.eteprc.faetec.rj.gov.br>
2026-08-13 14:54:28 -0300

    first commit

---
+++ subir.cmd
@@ -0,0 +1,7 @@
+echo "# aula-github" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:alison2521190431002-star/aula-github.git
+git push -u origin main
